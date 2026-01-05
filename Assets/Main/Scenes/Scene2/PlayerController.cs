24dd4bee1e036914e84fb16aec7d28a3548faa58
Dmitriy <levchenko.d.a1998@gmail.com>
2026-01-05 19:45:41 +0500

    added jump

--- Assets/Main/Scenes/Scene2/PlayerController.cs
+++ Assets/Main/Scenes/Scene2/PlayerController.cs
@@ -6,19 +6,47 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveForce = 50f;
     [SerializeField] float maxSpeed = 10f;
     [SerializeField] float friction = 5f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] Vector2 groundCheckOffset = new(0f, -1f);
+    [SerializeField] Vector2 groundCheckSize = new(0.9f, 0.2f);
+    [SerializeField] LayerMask groundLayer;
     private float _currentInput;
+    private bool _jumpRequested;
+    private bool _isGrounded;
     private const float InputThreshold = 0.01f;
 
     void Update()
     {
         _currentInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumpRequested = true;
+        }
     }
 
     void FixedUpdate()
     {
+        CheckGround();
         Move();
         ApplyFriction();
         ClampSpeed();
+
+        if (_jumpRequested)
+        {
+            if (_isGrounded)
+            {
+                Jump();
+            }
+            _jumpRequested = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _isGrounded ? Color.green : Color.red;
+        var boxCenter = (Vector2)transform.position + groundCheckOffset;
+        Gizmos.DrawWireCube(boxCenter, groundCheckSize);
     }
 
     private void Move()
@@ -32,6 +60,7 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyFriction()
     {
+        // if (!_isGrounded) return; // TODO
         bool isTurning = (_currentInput * body.linearVelocity.x) < 0;
         if (Mathf.Abs(_currentInput) < InputThreshold || isTurning)
         {
@@ -47,5 +76,16 @@ public class PlayerController : MonoBehaviour
         {
             body.linearVelocityX = maxSpeed * Mathf.Sign(velocity);
         }
+    }
+
+    private void CheckGround()
+    {
+        var origin = (Vector2)transform.position + groundCheckOffset;
+        _isGrounded = Physics2D.BoxCast(origin, groundCheckSize, 0f, Vector2.down, 0f, groundLayer);
+    }
+
+    private void Jump()
+    {
+        body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
